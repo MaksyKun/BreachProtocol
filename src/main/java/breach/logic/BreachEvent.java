e09aa68fb8a54110e1feeeff6c4961ae03b62ba7
MaksyKun <maksy.lp@web.de>
2023-12-20 19:03:08 +0100

    setup

--- src/main/java/breach/logic/BreachEvent.java
+++
@@ -1,8 +0,0 @@
-package breach.logic;
-
-public interface BreachEvent {
-    default void onBreachPreRunning(BreachTask breach) {}
-    default void onBreachPostRunning(BreachTask breach) {}
-    default void onBreachSuccess(BreachTask breach) {}
-    default void onBreachFailed(BreachTask breach) {}
-}
